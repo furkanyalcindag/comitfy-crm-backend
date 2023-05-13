e3e3573e12b6986cea32c9cfca5df033a0680c2e
furkanyalcindag <furkanyalcindag@gmail.com>
2023-05-13 20:50:29 +0300

    create proposal

--- src/main/java/com/comitfy/crm/app/service/ReferenceService.java
+++ src/main/java/com/comitfy/crm/app/service/ReferenceService.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ReferenceService {
 
     public List<ProposalStatusEnum> getProposalStatuses() {
 
-        List<ProposalStatusEnum> list = Arrays.stream(ProposalStatusEnum.values()).toList();
+        List<ProposalStatusEnum> list = Stream.of(ProposalStatusEnum.values())
+
+                .collect(Collectors.toList());
+
+
         list.remove(ProposalStatusEnum.APPROVED);
 
         return  list;
