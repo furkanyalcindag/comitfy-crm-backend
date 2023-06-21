0d0680945aa34382eb93bd8538c528cbd94ed0fc
furkanyalcindag <furkanyalcindag@gmail.com>
2023-06-21 16:30:26 +0300

    basedto

--- src/main/java/com/comitfy/crm/util/common/BaseDTO.java
+++ src/main/java/com/comitfy/crm/util/common/BaseDTO.java
@@ -3,7 +3,12 @@ package com.comitfy.crm.util.common;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.util.Date;
 import java.util.UUID;
 
 
@@ -21,5 +26,19 @@ public class BaseDTO {
 
     @JsonIgnore
     private UUID requestUserUUID;
+
+
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Date updatedDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String createdBy;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String lastModifiedBy;
 
 }
